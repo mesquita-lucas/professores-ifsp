a301fc34fefa2d3ff63bd3b48a2c7f97a5a8e9f5
Lucas de Mesquita <lucas.barros18@hotmail.com>
2025-03-13 20:23:52 -0300

    Project part 1/3

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./Banner.css";
+
+function Banner(){
+    return (
+
+        <header className='banner'>
+            <img src="/images/bannerifsp.png" alt="imagem aerea"/>
+        </header>
+
+    )
+}
+
+export default Banner;
---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    return (
+
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
+        </div>
+
+    )
+}
+
+export default CampoTexto
